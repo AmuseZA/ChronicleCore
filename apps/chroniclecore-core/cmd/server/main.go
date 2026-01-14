416c51d2b7049a94c91ac5a80d1954ad19470d66
AmuseZA <AmuseZA@users.noreply.github.com>
2026-01-14 22:54:45 +0200

    Bump to v1.8.5 for forced cache busting and robust build

--- apps/chroniclecore-core/cmd/server/main.go
+++ apps/chroniclecore-core/cmd/server/main.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	AppVersion          = "1.8.4"
+	AppVersion          = "1.8.5"
 	DefaultPort         = "8080"
 	MLPort              = 8081
 	UpdateCheckInterval = 30 * time.Minute
